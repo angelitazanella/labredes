1afe9b6b68746ebaae25771ae3e46fbcc789a95a
Angelita Rettore de Araujo Zanella <50925433+angelitazanella@users.noreply.github.com>
2022-04-11 12:52:58 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,10 +4,15 @@ if [ ! -e /opt/redes ];then
 	mkdir -p /opt/redes
 fi
 
-if [ ! -e /etc/init.d/clean.sh ]; then
-	echo "#!/bin/bash\n\nfind /home/aluno/ -type f -name *.pka -delete\nfind /home/aluno/ -type f -name *.pkt -delete" > /etc/init.d/clean.sh
-	`/usr/bin/chmod +x /ect/init.d/clean.sh`
-	`update-rc.d clean.sh defaults`
+if [ ! -e /opt/redes/clean.sh ]; then
+	echo "#!/bin/bash\n\nfind /home/aluno/ -type f -name *.pka -delete\nfind /home/aluno/ -type f -name *.pkt -delete" > /opt/redes/clean.sh
+	`/usr/bin/chmod +x /opt/redes/clean.sh`
+fi
+
+if [ ! -e /etc/cron.d/clean ]
+	echo "# /etc/cron.d/anacron: crontab entries for the anacron package\n\n
+SHELL=/bin/sh\nPATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin\n@reboot root /opt/redes/clean.sh\n" > /etc/cron.d/clean
+	`/usr/bin/chmod +x /etc/cron.d/clean`
 fi
 
 SCRIPTS=("vim" "traceroute" "mtr" "wireshark" "virtualbox")
