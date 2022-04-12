8641beff4ad904c1fe4ca093f9eeeaf5cc594b13
Angelita Rettore de Araujo Zanella <50925433+angelitazanella@users.noreply.github.com>
2022-04-12 09:53:37 -0300

    Atualizando script

--- update.sh
+++ update.sh
@@ -5,14 +5,29 @@ if [ ! -e /opt/redes ];then
 fi
 
 if [ ! -e /opt/redes/clean.sh ]; then
-	echo "#!/bin/bash\n\nfind /home/aluno/ -type f -name *.pka -delete\nfind /home/aluno/ -type f -name *.pkt -delete" > /opt/redes/clean.sh
+	echo "#!/bin/bash
+find /home/aluno/ -type f -name *.pka -delete
+find /home/aluno/ -type f -name *.pkt -delete" > /opt/redes/clean.sh
 	`/usr/bin/chmod +x /opt/redes/clean.sh`
 fi
 
 if [ ! -e /etc/cron.d/clean ]; then
-	echo "# /etc/cron.d/anacron: crontab entries for the anacron package\n\n
-SHELL=/bin/sh\nPATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin\n@reboot root /opt/redes/clean.sh\n" > /etc/cron.d/clean
+	echo "SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+@reboot root /opt/redes/clean.sh" > /etc/cron.d/clean
 	`/usr/bin/chmod +x /etc/cron.d/clean`
+fi
+
+if [ ! -e /home/aluno/PacketTracer.desktop ]; then
+echo"
+[Desktop Entry]
+Version=1.0
+Name=Cisco Packet Tracer
+Type=Application
+Exec=packettracer
+Icon=/opt/pt/art/app.png
+MimeType=application/x-pkz;application/x-pksz;application/x-pka;application/x-pkt;application/x-pks;x-scheme-handler/pttp
+" > /home/aluno/PacketTracer.desktop
 fi
 
 SCRIPTS=("vim" "traceroute" "mtr" "wireshark" "virtualbox")
